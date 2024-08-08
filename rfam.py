f41deeae13d16807e93456885d5b5d6d4eecfb6e
mmagnus <mmagnus@fas.harvard.edu>
2024-08-08 00:08:07 +0000

    rfam.py add simple wrapper

---
+++ rfam.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+
+"""
+from __future__ import print_function
+import argparse
+from icecream import ic
+import sys
+ic.configureOutput(outputFunction=lambda *a: print(*a, file=sys.stderr))
+ic.configureOutput(prefix='> ')
+import os
+
+def get_parser():
+    parser = argparse.ArgumentParser(
+        description=__doc__, formatter_class=argparse.RawDescriptionHelpFormatter)
+
+    #parser.add_argument('-', "--", help="", default="")
+
+    parser.add_argument("-v", "--verbose",
+                        action="store_true", help="be verbose")
+    parser.add_argument("file", help="", default="") # nargs='+')
+    return parser
+
+
+if __name__ == '__main__':
+    parser = get_parser()
+    args = parser.parse_args()
+
+    if list != type(args.file):
+        args.file = [args.file]
+
+        for f in args.file:
+
+            db = "/home/rnahub/rnahub/db/rfam/Rfam.cm"
+            #-o 
+            d = os.path.dirname(f)
+            cmd = "cmscan --tblout " + d + "/cmscan.tblout -o " + d + "/cmscan.out --noali " + db + " " + f
+            #families=`grep -v '^#' cmscan.tblout | head -n $max_rfam_num | uniq | awk '{print $2}' | sed -z 's/\n/|/g;s/|$/\n/'`
+            #echo "Rfam families:" $families
+            ic(cmd)
+            os.system(cmd)
+            with open(d + "/cmscan.out") as f:
+                print(f.read())
