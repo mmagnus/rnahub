d3c100d0d371bd2c419bd558ef41bef040435817
mmagnus <mag_dex@o2.pl>
2024-10-02 13:48:10 -0400

    up config

--- config.py
+++ config.py
@@ -6,6 +6,7 @@ db = "/n/home06/mmagnus/m/rnahub-db/vertebrates_185.fa"
 db = "/n/home06/mmagnus/m/rnahub-db/AY302558.1.fasta" #Echovirus_E6.fa"
 rscapedir = "/n/home06/mmagnus/m/opt/rscape_v2.0.4.b/"
 rscape = os.path.join(rscapedir, "bin/R-scape")
+RFAM_DB_PATH = "db/Rfam.cm"
 EASEL_PATH = ''
 if os.path.exists('config_local.py'):
     from config_local import *
