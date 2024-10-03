691ec1f5e2127267f7245a11d8871145d7af2a72
mmagnus <mmagnus@fas.harvard.edu>
2024-10-03 17:55:39 +0000

    update config for vm

--- config.py
+++ config.py
@@ -2,11 +2,11 @@
 import os
 DEBUG = True
 nhmmer = "/home/rnahub/opt/hmmer-3.3.2/bin/nhmmer"
-RFAM_DB_PATH = "/home/rnahub/db/Rfam.cm"
+RFAM_DB_PATH = "/home/rnahub/db/rfam/Rfam.cm"
 RSCAPE_DIR = "/home/rnahub/opt/rscape/rscape_v2.0.5"
 RSCAPE_PATH = os.path.join(RSCAPE_DIR, "bin/R-scape")
 EASEL_PATH = '/home/rnahub/opt/hmmer-3.3.2/bin/'
-
+INFERNAL_PATH = '/usr/bin/'
 if os.path.exists('config_local.py'):
     from config_local import *
 
