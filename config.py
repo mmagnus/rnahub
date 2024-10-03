50fa21a7b590aa8ac46ad55d14951636232daf5f
mmagnus <mmagnus@fas.harvard.edu>
2024-10-03 02:07:06 +0000

    config def for vm

--- config.py
+++ config.py
@@ -1,13 +1,12 @@
 # config.py
 import os
 DEBUG = True
-nhmmer = "/n/home06/mmagnus/m/opt/rscape_v2.0.4.b/lib/hmmer/src/nhmmer"
-db = "/n/home06/mmagnus/m/rnahub-db/vertebrates_185.fa"
-db = "/n/home06/mmagnus/m/rnahub-db/AY302558.1.fasta" #Echovirus_E6.fa"
-RFAM_DB_PATH = "db/Rfam.cm"
-RSCAPE_DIR = "/n/home06/mmagnus/m/opt/rscape_v2.0.4.b/"
+nhmmer = "/home/rnahub/opt/hmmer-3.3.2/bin/nhmmer"
+RFAM_DB_PATH = "/home/rnahub/db/Rfam.cm"
+RSCAPE_DIR = "/home/rnahub/opt/rscape/rscape_v2.0.5"
 RSCAPE_PATH = os.path.join(RSCAPE_DIR, "bin/R-scape")
-EASEL_PATH = ''
-SCRIPTS_DIR = ''
+EASEL_PATH = '/home/rnahub/opt/hmmer-3.3.2/bin/'
+
 if os.path.exists('config_local.py'):
     from config_local import *
+
