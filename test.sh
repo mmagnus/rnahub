d952346a6446873113b58c32f616e8bbbb759247
mmagnus <mmagnus@fas.harvard.edu>
2024-12-26 14:41:57 +0000

    up test.sh

--- test.sh
+++ test.sh
@@ -13,8 +13,38 @@ else
 fi
 unset __conda_setup
 # <<< conda initialize <<<
-python search_rfam.py example/example.fa example/gly1_query.fa example/xrrna.fa
+#cd /home/rnahub/rnahub-web
+cd /home/rnahub/rnahub
+blastn -num_threads 6 -db /home/rnahub/db/pdbnt/pdbnt -query /home/rnahub/rnahub-web/media/jobs//xrRNA-4488d320/seq.fa
+#python search_rfam.py ../rnahub-web/media/jobs/xrRNA-925d3bfd/seq.fa --job-path ../rnahub-web/media/jobs/xrRNA-925d3bfd
+#python daemon.py --debug xrRNA-d77b3fd7
+#cd /home/rnahub/rnahub-web/media/jobs/xrRNA_inferal_realig-d5291c8b #xrRNA-3505f7a5 # rnac_metagenomes-2961a93c
+#./search_rfam.py  example/xrrna.fa 
+#python search_rfam.py 
+#python rfam_hit.py RF04222 example/xrrna.fa
+#python daemon.py --debug CWC15-fab2e52a
+#xrRNA-d77b3fd7
+#GLY1_start_end-d978f32b
+#cd /home/rnahub/rnahub-web/media/jobs/CWC15-fab2e52a
+#CWC15_YDR163W_1kb-b6e0dadc
+#xrRNA_inferal_realig-d5291c8b #xrRNA-3505f7a5 # rnac_metagenomes-2961a93c
+#python ~/rnahub/rnahub.py --job-folder .
+#--dev-skip-nhmmer123 --rscape-path /home/rnahub/opt/rscape/rscape_v2.5.2/bin/R-scape
+
+#python ~/rnahub/rnahub.py --job-folder . --dev-skip-nhmmer123 --rscape-path /home/rnahub/opt/rscape/rscape_v2.5.2/bin/R-scape
+
+
+#python daemon.py --debug xrRNA_inferal_realig-d5291c8b #xrRNA-3505f7a5
+#~/rnahub/rnahub.py --job-folder '.' --dev-skip-nhmmer123 --db /home/rnahub/db/Riboviria_genome_db/20240826_all_Riboviria_db.fa
+#/home/rnahub/db/IMGVR-2022-12-19_7/IMGVR_all_nucleotides.fna
+
+exit
+#--job-folder '/home/rnahub'
+#./rnahub.py  --cpus 16 --repeatmasker  /home/rnahub/rnahub-web/media/jobs//xrRNA-ba7a2235/seq.fa --db /home/rnahub/db/Riboviria_genome_db/20240826_all_Riboviria_db.fa # --dev-skip-search
+
+#python search_rfam.py example/example.fa example/gly1_query.fa example/xrrna.fa
 #python search_blast.py --db pdbnt example/gly1_query.fa example/u6.fa
+./rnahub.py --job-folder jobs/ --cpus 16 example/gly1_query.fa --flanks-start 1 --flanks-end 830 --db /home/rnahub/db/1409_Acomycota_genomes-may19.fa
 exit
 #./rnahub.py --create-job-folder --cpus 16 --repeatmasker example/xrrna.fa --db /home/rnahub/db/Riboviria_genome_db/20240826_all_Riboviria_db.fa --dev-skip-search
 ./rnahub.py --create-job-folder --repeatmasker --cpus 16 example/gly1_query.fa --flanked example/gly1_flanked.fa --db /home/rnahub/db/1409_Acomycota_genomes-may19.fa
