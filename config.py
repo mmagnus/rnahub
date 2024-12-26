33570d61b48d6e873be9602c233d7730a7cee15a
mmagnus <mmagnus@fas.harvard.edu>
2024-12-26 14:36:24 +0000

    config RSCAPE_DIR = "/home/rnahub/opt/rscape/rscape_v2.5.2"

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 DEBUG = True
 nhmmer = "/home/rnahub/opt/hmmer/hmmer-3.3.2/bin/nhmmer"
 RFAM_DB_PATH = "/home/rnahub/db/rfam/Rfam.cm"
-RSCAPE_DIR = "/home/rnahub/opt/rscape/rscape_v2.0.5"
+RSCAPE_DIR = "/home/rnahub/opt/rscape/rscape_v2.5.2"
 RSCAPE_PATH = os.path.join(RSCAPE_DIR, "bin/R-scape")
 EASEL_PATH = '/home/rnahub/opt/hmmer/hmmer-3.3.2/bin/'
 INFERNAL_PATH = '/usr/bin/'
