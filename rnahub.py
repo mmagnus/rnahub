76bbf8ffa945692ccd892276a0ccebf11de40afa
mmagnus <mag_dex@o2.pl>
2026-03-11 12:33:27 -0400

    Fixed nhmmer commands that incorrectly concatenated query files with the database when piping to stdin
    rnahub.py: Fixed nhmmer commands that incorrectly concatenated query files with the database when piping to stdin

--- rnahub.py
+++ rnahub.py
@@ -281,12 +281,12 @@ def search(seq_path, seq_flanked_path = ''):
         # {j}/{fbase}.fa is causing missing organism problem
         flanking_evalue = args.evalue_for_flanking_search if args.evalue_for_flanking_search else args.evalue
         if args.flanks_in_header or args.flanks_start:
-            cmd = f"time cat {job_path}/{fbase}.fa {db} | {nhmmer} {dna} --noali --cpu {CPUs} --incE {flanking_evalue} -A {job_path}/v0.sto {job_path}/{fbase}.fa - "#| tee {j}/v0.out"
+            cmd = f"time cat {db} | {nhmmer} {dna} --noali --cpu {CPUs} --incE {flanking_evalue} -A {job_path}/v0.sto {job_path}/{fbase}.fa - "#| tee {j}/v0.out"
             if '.gz' in db:
                 cmd = f"time zcat {db} | {nhmmer} {dna} --noali --cpu {CPUs} --incE {flanking_evalue} -A {job_path}/v0.sto {job_path}/{fbase}.fa - "#| tee {j}/v0.out"
         else:
             # overwrite with seq_flanked_masked_path if needed
-            cmd = f"time cat {seq_flanked_path} {db} | {nhmmer} {dna} --noali --cpu {CPUs} --incE {flanking_evalue} -A {job_path}/v0.sto {seq_flanked_path} - "#| tee {job_path}/v0.out"
+            cmd = f"time cat {db} | {nhmmer} {dna} --noali --cpu {CPUs} --incE {flanking_evalue} -A {job_path}/v0.sto {seq_flanked_path} - "#| tee {job_path}/v0.out"
             if '.gz' in db:
                 cmd = f"time zcat {db} | {nhmmer} {dna} --noali --cpu {CPUs} --incE {flanking_evalue} -A {job_path}/v0.sto {seq_flanked_path} - "#| tee {job_path}/v0.out"
 
@@ -313,7 +313,7 @@ def search(seq_path, seq_flanked_path = ''):
                 evalue =  args.evalue_final
             #  {j}/{fbase}.fa
             if i == 1: # add seq only if first iteration
-                command = f" time cat {db} {input_file} | {nhmmer} {dna} --noali --cpu {CPUs} -E {args.reporting_evalue} --incE {evalue} -A {job_path}/{sto_file} {input_file} - "#| tee {j}/{output_file}"
+                command = f" time cat {db} | {nhmmer} {dna} --noali --cpu {CPUs} -E {args.reporting_evalue} --incE {evalue} -A {job_path}/{sto_file} {input_file} - "#| tee {j}/{output_file}"
             else:
                 command = f" time cat {db} | {nhmmer} {dna} --noali --cpu {CPUs} -E {args.reporting_evalue} --incE {evalue} -A {job_path}/{sto_file} {input_file} - "#| tee {j}/{output_file}"
 
